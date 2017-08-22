f5a1b90512d069e30f3d6ba73a1a1825525d5c57
791127866@qq.com <791127866@qq.com>
2017-08-22 11:28:11 +0800

    init source

---
+++ mylibrary/src/main/java/com/topvision/mylibrary/CustomUtil.java
@@ -0,0 +1,13 @@
+package com.topvision.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/8/22.
+ */
+
+public class CustomUtil {
+    public static void test() {
+        Log.e("tvLog", "test");
+    }
+}
